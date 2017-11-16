1fd62ef7ff249a1146f8cd170abf013546d49d14
s4m_s3pi0l <thomas.chaumont.pro@gmail.com>
2017-11-16 11:51:32 +0100

    debut de déclaration de classe Image

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@
 
 using namespace std;
 
+class image {
+private:
+    string entete;
+    string commentaire;
+    int maxVal;
+    int longueur;
+    int largeur;
+    // creer tableau de valeurs ici
+};
+
 int main() {
 
 }
