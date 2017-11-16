d23cf12844ee8c13a2e867edf71f5d6c1824ac26
Johnny Hoang <hoang.joh@gmail.com>
2017-11-16 11:54:04 +0100

    Seuillage.cpp
    v0.1

---
+++ Seuillage.cpp
@@ -0,0 +1,16 @@
+// TP2MEDEV.cpp : Définit le point d'entrée pour l'application console.
+//
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class PGM;
+
+PGM seuillage(string image, int seuil) {
+	
+	PGM image = lecture(image);
+
+}
+
