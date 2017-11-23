e348630dd919b8be92628b87f0b6ed6974229efa
LouisLN <Crashdufise@gmail.com>
2017-11-23 08:41:19 +0100

    Début de la fonction constructrice
    Ecriture de la fonction constructrice, il reste la partie contenu

--- Lecture_Ecriture.cpp
+++ Lecture_Ecriture.cpp
@@ -8,7 +8,33 @@ void image::ecriture(string nomFichier) {
 }
 
 image::image(string nomFichier) {
+    ifstream fichier(nomFichier, ios::in);
 
+    if(fichier) { // n'entre que si l'ouverture a réussi
+
+        string ligne;
+
+        //compter les lignes
+        int nb_lignes = 0;
+        while(!fichier.eof()){
+            getline(fichier,ligne);
+            nb_lignes++;
+            fichier.close();
+        }
+        ifstream fichier(nomFichier, ios::in);
+
+        getline(fichier, ligne);
+        entete = ligne;
+        // on a rentré le "P2" normalement
+
+        getline(fichier, ligne);
+        commentaire = ligne;
+        // on a rentré le commentaire normalement
+
+        for(int i = 2; i < nb_lignes; i++) {
+            getline(fichier, ligne);
+        }
+    }
 }
 
 vector<vector<int>> getContenu() {
