8f0342cc31eae5823871c26c1e0adc4cfedab7dd
BichAnh.H <32197171+Kiruko@users.noreply.github.com>
2017-11-23 10:02:40 +0100

    Fin de la fonction difference

--- Seuillage.cpp
+++ Seuillage.cpp
@@ -44,6 +44,8 @@ void difference(string filepath1, string filepath2) {
 			tabPGM[i][j].push_back(tabPGM1[i][j]-tabPGM2[i][j]));
 		}
 	}
+	image resultat = image(imagePGM.getNom(), "Différence entre les images suivantes : \n" + imagePGM1.getNom() + "\n" + imagePGM2.getNom(), hauteur, largeur, tabPGM);
+	resultat.ecriture();
 }
 
 
